e064ae1102b8358fca4c9107a35699d9d36c7600
JarkaP <jarka.p@gmail.com>
2022-02-28 17:35:26 +0100

    add button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     content: ['./index.html'],
-    theme: {},
+    theme: {
+        extend: {
+            boxShadow: {
+                bottom: 'inset 0 0 0 1px rgba(0, 0, 0, 0.1), inset 0 -0.25em 0 rgba(0, 0, 0, 0.25), 0 0.25em 0.25em rgba(0, 0, 0, 0.05)',
+            },
+        },
+    },
     plugins: [],
 };
